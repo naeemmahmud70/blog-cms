3d7c22b58f5290526749b0ac1de785781c2a2462
naeemmahmud70 <naeemmahmud370@gmail.com>
2025-08-17 17:53:38 +0600

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": {},
-    global: {},
-  },
-  resolve: {
-    alias: {
-      "util": "rollup-plugin-node-polyfills/polyfills/util",
-    },
-  },
+  base: "/",
 });
