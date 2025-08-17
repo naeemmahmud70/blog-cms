1e3f8ba346f1752dffe1ae993cfacffcf532019b
naeemmahmud70 <naeemmahmud370@gmail.com>
2025-08-17 14:55:19 +0600

    Fix SPA routing for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["quill", "react-quilljs"], // pre-bundle QuillJS & react-quilljs
+  },
+  ssr: {
+    noExternal: ["quill", "react-quilljs"], // prevents Vite from leaving Node-only code in bundle
+  },
 });
